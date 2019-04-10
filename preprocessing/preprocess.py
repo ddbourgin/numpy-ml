7d0ceea4e1a149f9072b6970e9895d79c38d0a64
David Bourgin <ddbourgin@gmail.com>
2019-04-10 18:55:27 -0400

    add nearest neighbor interpolation

--- preprocessing/preprocess.py
+++ preprocessing/preprocess.py
@@ -1,10 +1,14 @@
 import numpy as np
 
+#######################################################################
+#                          Signal Resampling                          #
+#######################################################################
+
 
 def batch_resample(X, new_dim, mode="bilinear"):
     """
-    Resample each image in a batch to `new_dim` using the specified resampling
-    strategy.
+    Resample each image (or similar grid-based 2D signal) in a batch to
+    `new_dim` using the specified resampling strategy.
 
     Parameters
     ----------
@@ -13,15 +17,20 @@ def batch_resample(X, new_dim, mode="bilinear"):
     new_dim : 2-tuple of (out_rows, out_cols)
         The dimension to resample each image to
     mode : str
-        The resampling strategy to employ. Valid entries are {'bilinear'}
+        The resampling strategy to employ. Valid entries are {'bilinear',
+        'neighbor'}
 
     Returns
     -------
     resampled : numpy array of shape (n_ex, out_rows, out_cols, in_channels)
         The resampled image volume
     """
-    if mode != "bilinear":
-        raise NotImplementedError
+    if mode == "bilinear":
+        interpolate = bilinear_interpolate
+    elif mode == "neighbor":
+        interpolate = nn_interpolate_2D
+    else:
+        raise NotImplementedError("Unrecognized resampling mode: {}".format(mode))
 
     out_rows, out_cols = new_dim
     n_ex, in_rows, in_cols, n_in = X.shape
@@ -33,16 +42,17 @@ def batch_resample(X, new_dim, mode="bilinear"):
     # resample each image
     resampled = []
     for i in range(n_ex):
-        r = bilinear_interpolate(X[i, ...], x, y)
+        r = interpolate(X[i, ...], x, y)
         r = r.reshape(out_rows, out_cols, n_in)
         resampled.append(r)
     return np.dstack(resampled)
 
 
-def bilinear_interpolate(X, x, y):
+def nn_interpolate_2D(X, x, y):
     """
-    Estimates of the pixel values at the coordinates (x, y) in X via bilinear
-    interpolation. Modified from: https://bit.ly/2NMb1Dr
+    Estimates of the pixel values at the coordinates (x, y) in X using a
+    nearest neighbor interpolation strategy. Assumes the current entries
+    in X reflect equally-spaced samples from a 2D integer grid.
 
     Parameters
     ----------
@@ -55,7 +65,58 @@ def bilinear_interpolate(X, x, y):
 
     Returns
     -------
-    samples : list of length k
+    samples : numpy array of shape (k, in_channels)
+        The samples for each (x,y) coordinate computed via nearest neighbor
+        interpolation
+    """
+    nx, ny = np.around(x), np.around(y)
+    nx = np.clip(nx, 0, X.shape[1] - 1).astype(int)
+    ny = np.clip(ny, 0, X.shape[0] - 1).astype(int)
+    return X[ny, nx, :]
+
+
+def nn_interpolate_1D(X, t):
+    """
+    Estimates of the signal values at X[t] using a nearest neighbor
+    interpolation strategy.
+
+    Parameters
+    ----------
+    X : numpy array of shape (in_length, in_channels)
+        An input image sampled along an integer `in_length`
+    t : list of length k
+        A list of coordinates for the samples we wish to generate
+
+    Returns
+    -------
+    samples : numpy array of shape (k, in_channels)
+        The samples for each (x,y) coordinate computed via nearest neighbor
+        interpolation
+    """
+    nt = np.clip(np.around(t), 0, X.shape[0] - 1).astype(int)
+    return X[nt, :]
+
+
+def bilinear_interpolate(X, x, y):
+    """
+    Estimates of the pixel values at the coordinates (x, y) in X via bilinear
+    interpolation. Assumes the current entries in X reflect equally-spaced
+    samples from a 2D integer grid.
+
+    Modified from: https://bit.ly/2NMb1Dr
+
+    Parameters
+    ----------
+    X : numpy array of shape (in_rows, in_cols, in_channels)
+        An input image sampled along a grid of `in_rows` by `in_cols`.
+    x : list of length k
+        A list of x-coordinates for the samples we wish to generate
+    y : list of length k
+        A list of y-coordinates for the samples we wish to generate
+
+    Returns
+    -------
+    samples : list of length (k, in_channels)
         The samples for each (x,y) coordinate computed via bilinear
         interpolation
     """
@@ -82,26 +143,43 @@ def bilinear_interpolate(X, x, y):
     return (Ia * wa).T + (Ib * wb).T + (Ic * wc).T + (Id * wd).T
 
 
-def DFT(signal, sample_rate=440000):
+#######################################################################
+#                        Fourier Decomposition                        #
+#######################################################################
+
+
+def DFT(frame, fs=44000):
     """
-    1D discrete Fourier transform (DFT) for real-valued input.
+    A naive implementation of the 1D discrete Fourier transform (DFT) for
+    a real-valued input.
+
+    Parameters
+    ----------
+    frame : numpy array of shape (N,)
+        A signal frame consisting of N samples
+    fs : int (defalt: 44000)
+        The sample rate/frequency for the signal
+
+    Returns
+    -------
+    spectrum : numpy array of shape (N,)
+        The coefficients of the frequency spectrum for `frame`, including
+        imaginary components.
+    bins : numpy array of shape (N,)
+        The frequency bin centers associated with each coefficient in the
+        dft spectrum.
     """
-    N = len(signal)  # window length
-    d = 1 / sample_rate  # sample spacing
+    N = len(frame)  # window length
 
-    # compute basis vectors
-    F = np.zeros((N, N))
-    for k in range(N):
-        for t in range(N):
-            # basis vectors x frequency bins
-            F[k, t] = np.exp(-1j * t * k * (2 * np.pi / N))
+    # F[i,j] = coefficient for basis vector i, timestep j
+    F = np.arange(N).reshape(1, -1) * np.arange(N).reshape(-1, 1)
+    F = np.exp(F * (-1j * 2 * np.pi / N))
 
-    # compute spectrum
-    spectrum = np.zeros(N)
-    for i in range(F.shape[-1]):
-        spectrum[i] = np.abs(np.vdot(F[i, :], signal))  # complex dot product
+    # vdot only operates on vectors (rather than ndarrays), so we have to
+    # loop over each basis vector in F explicitly
+    spectrum = np.array([np.vdot(f, frame) for f in F])
 
-    pos = list(range(0, (N - 1) / 2 if N % 2 else (N / 2) - 1))
-    neg = list(range(-(N - 1) / 2 if N % 2 else -N / 2, -1))
-    freq_bins = np.array(pos + neg) * d * N
+    # calc frequency bin centers
+    l, r = (1 + (N - 1) / 2, (1 - N) / 2) if N % 2 else (N / 2, -N / 2)
+    freq_bins = np.r_[np.arange(l), np.arange(r, 0)] * fs / N
     return spectrum, freq_bins
