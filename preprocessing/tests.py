ca26bf9b1bc280f64c1e213bc49da0449ed7bcb7
David Bourgin <ddbourgin@gmail.com>
2019-07-04 02:47:27 -0400

    add test for tdif

--- preprocessing/tests.py
+++ preprocessing/tests.py
@@ -1,29 +1,27 @@
+import sys
 from collections import Counter
-import numpy as np
-
-from general import Standardizer
-from nlp import HuffmanEncoder
 
 import huffman
+import numpy as np
+
+from sklearn.datasets import fetch_20newsgroups
 from sklearn.preprocessing import StandardScaler
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+from nlp import HuffmanEncoder, TFIDFEncoder
+from general import Standardizer
 
 
-def random_paragraph(n_words):
-    words = [
-        "lorem",
-        "ipsum",
-        "dolor",
-        "sit",
-        "amet",
-        "consetetur",
-        "sadipscing",
-        "elitr",
-        "sed",
-        "diam",
-        "nonumy",
-        "eirmod",
-    ]
-    return [np.random.choice(words) for _ in range(n_words)]
+sys.path.append("..")
+from utils.testing import random_paragraph
+
+
+def load_newsgroups():
+    categories = ["alt.atheism", "talk.religion.misc", "comp.graphics", "sci.space"]
+    dataset = fetch_20newsgroups(
+        subset="all", categories=categories, shuffle=True, random_state=42
+    )
+    return dataset.data, dataset.target
 
 
 def test_huffman():
@@ -58,4 +56,40 @@ def test_standardizer():
         gold = theirs.fit_transform(X)
 
         np.testing.assert_almost_equal(mine, gold)
+        print("PASSED")
+
+
+def test_tfidf():
+    while True:
+        docs = []
+        n_docs = np.random.randint(1, 10)
+        for d in range(n_docs):
+            n_lines = np.random.randint(1, 1000)
+            lines = [random_paragraph(np.random.randint(1, 10)) for _ in range(n_lines)]
+            docs.append("\n".join([" ".join(l) for l in lines]))
+
+        smooth = bool(np.random.randint(2))
+
+        tfidf = TFIDFEncoder(
+            lowercase=True,
+            min_count=0,
+            smooth_idf=smooth,
+            max_tokens=None,
+            input_type="strings",
+            filter_stopwords=False,
+        )
+        gold = TfidfVectorizer(
+            input="content",
+            norm=None,
+            use_idf=True,
+            lowercase=True,
+            smooth_idf=smooth,
+            sublinear_tf=False,
+        )
+
+        tfidf.fit(docs)
+        mine = tfidf.transform(ignore_special_chars=True)
+        theirs = gold.fit_transform(docs).toarray()
+
+        np.testing.assert_almost_equal(mine, theirs)
         print("PASSED")
