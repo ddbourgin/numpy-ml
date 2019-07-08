6daf7935285bf3547e9bdd047006f910946482d7
WuZhuoran <munoliver007@gmail.com>
2019-07-08 10:59:12 -0700

    feat: add PRelu activation function

--- neural_nets/activations/activations.py
+++ neural_nets/activations/activations.py
@@ -286,3 +286,46 @@ class Hard_Sigmoid(ActivationBase):
     def grad2(self, x):
         return np.zeros_like(x)
 
+
+class PReLU(ActivationBase):
+    """Parametric Rectified Linear Unit.
+    It follows:
+    `f(x) = alpha * x for x < 0`,
+    `f(x) = x for x >= 0`,
+    where `alpha` is a learned array with the same shape as x.
+    # Input shape
+        z: Input Array
+    # Output shape
+        Same shape as the input.
+    # Arguments
+        alpha_initializer: initializer function for the weights.
+        alpha_regularizer: regularizer for the weights.
+        alpha_constraint: constraint for the weights.
+        shared_axes: the axes along which to share learnable
+            parameters for the activation function.
+            For example, if the incoming feature maps
+            are from a 2D convolution
+            with output shape `(batch, height, width, channels)`,
+            and you wish to share parameters across space
+            so that each filter only has one set of parameters,
+            set `shared_axes=[1, 2]`.
+    # References
+        - [Delving Deep into Rectifiers: Surpassing Human-Level Performance on
+           ImageNet Classification](https://arxiv.org/abs/1502.01852)
+    """
+
+    def __init__(self, alpha=0.3):
+        self.alpha = alpha
+        super().__init__()
+
+    def __str__(self):
+        return "PReLU(alpha={})".format(self.alpha)
+
+    def fn(self, z):
+        return np.where(z < 0, self.alpha * z, z)
+
+    def grad(self, x):
+        return np.where(x < 0, np.ones_like() * self.alpha, np.ones_like())
+
+    def grad2(self, x):
+        return np.zeros_like(x)
