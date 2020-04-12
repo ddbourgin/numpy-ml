049d354fc4a48ca929f7c0ddfe6968f4466fbc01
ddbourgin <dbourgin@gmail.com>
2020-04-11 22:31:37 -0400

    add documentation to for module

--- numpy_ml/neural_nets/losses/__init__.py
+++ numpy_ml/neural_nets/losses/__init__.py
@@ -1,1 +1,8 @@
+"""
+Common neural network loss functions.
+
+This module implements loss objects that can be used during neural network
+training.
+"""
+
 from .losses import *
--- numpy_ml/neural_nets/tests/__init__.py
+++ numpy_ml/neural_nets/tests/__init__.py
@@ -1,1 +1,8 @@
+"""
+A module of tests for many of the components in the neural_nets package.
+
+Note that many of the tests in this module rely on external packages like
+PyTorch and Tensorflow for gold-standard implementations.
+"""
+
 from .tests import *
