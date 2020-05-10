0178e9935caf9ebdcd342901c545a9e38d21b234
ddbourgin <ddbourgin@gmail.com>
2020-05-10 01:40:26 -0400

    update copyright year

--- docs/conf.py
+++ docs/conf.py
@@ -24,7 +24,7 @@ gh_url = "https://github.com/ddbourgin/numpy-ml"
 # -- Project information -----------------------------------------------------
 
 project = "numpy-ml"
-copyright = "2019, David Bourgin"
+copyright = "2020, David Bourgin"
 author = "David Bourgin"
 
 # The short X.Y version
