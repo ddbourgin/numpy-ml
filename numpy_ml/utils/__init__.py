8b3f72e31575c9b96e25fa7121a18546b261dafd
ddbourgin <ddbourgin@gmail.com>
2020-06-21 16:01:10 -0400

    add a misc utils file for common math functions

--- numpy_ml/utils/__init__.py
+++ numpy_ml/utils/__init__.py
@@ -1,6 +1,9 @@
+"""Utilities module"""
+
 from . import testing
 from . import data_structures
 from . import distance_metrics
 from . import kernels
 from . import windows
 from . import graphs
+from . import misc
