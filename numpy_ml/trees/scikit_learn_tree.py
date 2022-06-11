cca1baaca1a89abde38e3b786c6cf917d44138de
bruce <>
2022-06-11 20:05:51 +0800

    new code info

--- numpy_ml/trees/scikit_learn_tree.py
+++ numpy_ml/trees/scikit_learn_tree.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 from sklearn.tree import DecisionTreeRegressor
 from sklearn import linear_model
 
-# Data set
+# Data set，skl官方代码给出样例
 x = np.array(list(range(1, 11))).reshape(-1, 1)
 y = np.array([5.56, 5.70, 5.91, 6.40, 6.80, 7.05, 8.90, 8.70, 9.00, 9.05]).ravel()
 
